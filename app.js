f2ac2f357b748f43395a87b6b3f8fc76b6d7fa4c
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-02-07 14:02:00 +0900

    [Update] HTMLファイルを出力

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ var http = require('http').Server(app);
 const PORT = process.env.PORT || 7000;
 
 app.get('/' , function(req, res){
-   res.send('hello world');
+   res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(PORT, function(){
   console.log('server listening. Port:' + PORT);
 });
-
